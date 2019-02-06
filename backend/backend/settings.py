32a41a855c9725f020c913846b26b39ccf9f3ffe
Jonathan Adelson <jon@adelson.io>
2019-02-06 16:10:24 +0000

    Creates frontend section with create-react-app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ from .settings_local import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dev04.adelson.io']
 
 
 # Application definition
