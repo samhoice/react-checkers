bf9c9d3c45b0008d500336f046b203e767d64446
Jonathan Adelson <jon@adelson.io>
2019-02-06 18:11:18 +0000

    Adds django extensions and fixes settings to serve react static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -108,5 +109,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/skele/static/'
 
+STATICFILES_DIRS = [
+    ('frontend', '/home/jon/skeleton/frontend/build/'),
+]
